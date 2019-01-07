4650b7136a9f5ca2ffea49b9b7c465f80951d718
nautatva <nnautatva@gmail.com>
2019-01-07 22:11:22 +0530

    fontello for footer icons

--- home/js/flip.js
+++ home/js/flip.js
@@ -1,55 +1,60 @@
+function setIntervalI(func, interval) {
+  func();
+  return setInterval(func, interval);
+}
+
 function flip() {
   setTimeout(() => {
-    setInterval(function() {
+    setIntervalI(function() {
       $('#js-flip-1 .card1').toggleClass('flipped');
     }, 5000);
   }, 10);
 
   setTimeout(() => {
-    setInterval(function() {
+    setIntervalI(function() {
       $('#js-flip-2 .card1').toggleClass('flipped');
     }, 5000);
   }, 2500);
 
   setTimeout(() => {
-    setInterval(function() {
+    setIntervalI(function() {
       $('#js-flip-3 .card1').toggleClass('flipped');
     }, 5000);
   }, 1500);
 
   // row 2
   setTimeout(() => {
-    setInterval(function() {
+    setIntervalI(function() {
       $('#js-flip-4 .card1').toggleClass('flipped');
     }, 5000);
   }, 500);
 
   setTimeout(() => {
-    setInterval(function() {
+    setIntervalI(function() {
       $('#js-flip-6 .card1').toggleClass('flipped');
     }, 5000);
   }, 3700);
 
   // row 3
   setTimeout(() => {
-    setInterval(function() {
+    setIntervalI(function() {
       $('#js-flip-7 .card1').toggleClass('flipped');
     }, 5000);
   }, 1200);
 
   setTimeout(() => {
-    setInterval(function() {
+    setIntervalI(function() {
       $('#js-flip-8 .card1').toggleClass('flipped');
     }, 5000);
   }, 4340);
 
   // setTimeout(() => {
-    setInterval(function() {
-      $('#js-flip-9 .card1').toggleClass('flipped');
-      console.log('this');
-    }, 5000);
+  setIntervalI(function() {
+    $('#js-flip-9 .card1').toggleClass('flipped');
+    console.log('this');
+  }, 5000);
   // }, 100);
-  console.log('start');
+  console.log('starggggt');
 }
 
 $(document).ready(function() {
