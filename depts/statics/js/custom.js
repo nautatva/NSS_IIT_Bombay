35a27ae593a7b1adb681955e83508d2605d92693
nautatva <nnautatva@gmail.com>
2019-01-11 20:19:59 +0530

    gallery optimised and changed to html

--- depts/statics/js/custom.js
+++ depts/statics/js/custom.js
@@ -1,11 +1,11 @@
-$('.portfolio-modal').on('hidden.bs.modal', function(e) {
-  $(function() {
-    $.scrollify({
-      section: '.scrollify',
-      setHeights: false
-    });
-  });
-});
+// $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+//   $(function() {
+//     $.scrollify({
+//       section: '.scrollify',
+//       setHeights: false
+//     });
+//   });
+// });
 
 $('.portfolio-modal').on('shown.bs.modal', function(e) {
   $.scrollify.destroy();
