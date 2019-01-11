87d9651fe746a3ec3a42699e14b00a2975574481
nautatva <nnautatva@gmail.com>
2019-01-11 20:19:59 +0530

    gallery optimised and changed to html

--- gallery/js/overlay.js
+++ gallery/js/overlay.js
@@ -1,21 +1,21 @@
-function overlay() {
-  $('.overlay').show();
-  changeImg();
-}
-let i=0;
-const image = ['image1.jpg','image2.jpg','image3.jpg','image4.jpg' ];
-const time=2000;
+// function overlay() {
+//   $('.overlay').show();
+//   changeImg();
+// }
+// let i=0;
+// const image = ['image1.jpg','image2.jpg','image3.jpg','image4.jpg' ];
+// const time=2000;
 
 
-function changeImg(){
+// function changeImg(){
     
-    document.slide.src=image[i];
+//     document.slide.src=image[i];
 
-    if (i<image.length-1) {
-        i++;
-    }else{
-        i=0;
-    }
+//     if (i<image.length-1) {
+//         i++;
+//     }else{
+//         i=0;
+//     }
     
-    setTimeout("changeImg()",time);
-}
+//     setTimeout("changeImg()",time);
+// }
