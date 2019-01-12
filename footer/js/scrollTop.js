26e4c6010099688abbd4ae18bc9dd57ece7ae5a6
nautatva <nnautatva@gmail.com>
2019-01-12 09:34:21 +0530

    added Shbham's footer and department codes merged - 2

---
+++ footer/js/scrollTop.js
@@ -0,0 +1,16 @@
+(function($) {
+  //Check to see if the window is top if not then display button
+  jQuery(window).scroll(function() {
+    if (jQuery(this).scrollTop() > 300) {
+      jQuery('.scrollToTop').fadeIn();
+    } else {
+      jQuery('.scrollToTop').fadeOut();
+    }
+  });
+
+  //Click event to scroll to top
+  jQuery('.scrollToTop').click(function() {
+    jQuery('html, body').animate({ scrollTop: 0 }, 800);
+    return false;
+  });
+})(jQuery);
