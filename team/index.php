12e6a1d0d1a5e92780b5a9bdabd200c7226875e8
Atharv Kotwal <atharvkotwal@gmail.com>
2019-01-26 22:38:00 +0530

    Added PHP to Team Page

--- team/index.php
+++ team/index.php
@@ -1,3 +1,46 @@
+<?php
+$host ="10.105.177.5";
+$username = "nss";
+$password = "nssiitb@2015";
+$db = "nss";
+$conn = mysqli_connect($host, $username, $password, $db)  or die("Couldn't connect to Server");
+
+$depts=$conn->query("SELECT distinct dept FROM Coreteam1819 ") ;
+$mydepts = [];
+$t=0;
+while($mydept = $depts->fetch_assoc()){
+  switch ($mydept['dept']) {
+    case 'OC':
+      $name = 'Overall Coordinators';
+      break;
+    case 'EO':
+      $name = 'Educational Outreach';
+      break;
+    case 'SSD':
+      $name = 'Sustainable Social Development';
+      break;
+    case 'GC':
+      $name = 'Green Campus';
+      break;
+    case 'NIC':
+      $name = 'National Innovation Cell';
+      break;
+    case 'Media':
+      $name = 'Media & Design';
+      break;        
+    default:
+      $name = 'Web & Finance';
+      break;
+  }
+  $y = array('deptcode' => $mydept['dept'],
+             'deptname' => $name 
+);
+  array_push($mydepts, $y);
+}
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -11,6 +54,46 @@
 
   <body>
     <link rel="stylesheet" href="./css/ssd.css" />
+    <h1 class="title" >
+      NSS Core-Team 2018-19</h1>
+    <?php
+    foreach ($mydepts as $q) {
+      $code = $q['deptcode'];
+      $sql1 = $conn->query("SELECT * FROM Coreteam1819 WHERE dept='$code' ");
+      echo('
+      <h1 class="dtitle">'.$q['deptname'].'</h1>
+      <div id="ssd">
+      ');
+      while($details = $sql1->fetch_assoc()){
+        echo('
+        <div class="member">
+        <div class="team-member">
+          <img
+            src="'.$details['photo'].'"
+            alt="'.$details['COL 1'].'"
+          />
+          <div class="team-hover">
+            <div class="desk">
+              <h4>Hi There !</h4>
+              <p>introduce yourself</p>
+            </div>
+            <div class="s-link">
+              <i class="icon " aria-hidden="true">&#xf09a;</i>
+              <i class="icon " aria-hidden="true">&#xe801;</i>
+              <i class="icon " aria-hidden="true">&#xe800;</i>
+            </div>
+          </div>
+        </div>
+
+        <div class="team-title">
+          <h5>'.$details['COL 1'].'</h5>
+        </div>
+      </div>
+        ');
+      }
+      echo('</div>');
+    }
+    ?>
     <div id="ssd">
       <!-- member 1 -->
       <div class="member">
@@ -40,6 +123,9 @@
     </div>
 
 
+
+
+<!--FOOTER -->
     
     <link rel="stylesheet" href="./css/footer.css" />
     <footer id="footer">
