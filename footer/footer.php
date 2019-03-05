8dfc8b1a9fe132e6c97c48b5c738f0692c5a37c1
nautatva <nnautatva@gmail.com>
2019-03-05 22:07:16 +0530

    new history page edit

---
+++ footer/footer.php
@@ -0,0 +1,36 @@
+<link rel="stylesheet" href="../footer/footer.css" />
+<footer id="footer">
+  <p class="footer-links">
+    <!-- <a href="#">Home</a> · <a href="#">Blog</a> · <a href="#">Team</a> · -->
+    <!-- <a href="#">Give Feedback</a> -->
+  </p>
+  <div id="footer-icons" class="wrapper">
+    <ul>
+      <li class="facebook">
+        <i class="icon fb" aria-hidden="true">&#xf09a;</i>
+        <div class="slider"><p>facebook</p></div>
+      </li>
+
+      <li class="twitter">
+        <i class="icon tweet" aria-hidden="true">&#xf099;</i>
+        <div class="slider"><p>twitter</p></div>
+      </li>
+
+      <li class="instagram">
+        <i class="icon insta" aria-hidden="true">&#xf16d;</i>
+        <div class="slider"><p>instagram</p></div>
+      </li>
+
+      <li class="google">
+        <i class="icon youtubeee" aria-hidden="true">&#xf16a;</i>
+        <div class="slider"><p>youtube</p></div>
+      </li>
+
+      <li class="whatsapp">
+        <i class="icon wordpresssss" aria-hidden="true">&#xf19a;</i>
+        <div class="slider"><p>wordpress</p></div>
+      </li>
+    </ul>
+  </div>
+  <p id="copyright">Copyright © NSS, IIT Bombay, 2018</p>
+</footer>
---
+++ navbar/navbar.php
@@ -0,0 +1,174 @@
+<!-- Navbar -->
+<section id="nav" class="no">
+  <link rel="stylesheet" href="../navbar/navbar.css" />
+  <img id="nav-logo" src="../assets/NSS/logo128.png" />
+
+  <input id="burger" type="checkbox" style="display:none;" />
+
+  <label for="burger"> <span></span> <span></span> <span></span> </label>
+
+  <h1 id="colorchange">NSS, IIT Bombay</h1>
+
+  <script src="../navbar/navbar.js"></script>
+  <nav class="cbp-hrmenu">
+    <ul>
+      <li class="cbp-hropen">
+        <div class="cbp-hrsub">
+          <div class="cbp-hrsub-inner">
+            <div class="nav-mobile">
+              <a href="#">Departments</a>
+              <ul>
+                <li>
+                  <a href="https://gymkhana.iitb.ac.in/~nss/tem/depts/EO/"
+                    >Educational Outreach</a
+                  >
+                </li>
+                <li>
+                  <a href="https://gymkhana.iitb.ac.in/~nss/tem/depts/GC"
+                    >Green Campus</a
+                  >
+                </li>
+                <li>
+                  <a href="https://gymkhana.iitb.ac.in/~nss/tem/depts/SSD/"
+                    >Sustainable Social Development</a
+                  >
+                </li>
+                <li><a href="#">Media and Design</a></li>
+                <li><a href="#">Web</a></li>
+              </ul>
+            </div>
+            <div class="nav-mobile">
+              <a href="#">Youtube</a>
+              <ul>
+                <li>
+                  <a
+                    href="https://www.youtube.com/playlist?list=PL7jZaz9RpobeLA6ZE5e-Is4HoLkADcdbj"
+                    >Greenopedia</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="https://www.youtube.com/channel/UCiRgHJGPSMsMI3hcQKrm4yA"
+                    >Open Learning Initiative - OLI</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="https://www.youtube.com/channel/UCskd3K0NyyHqZ1t7AMAYACQ"
+                    >Tarang</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="https://www.youtube.com/channel/UC2iPnxGyKViqjV37f3r6uJw"
+                    >Voice For Purpose - VFP</a
+                  >
+                </li>
+              </ul>
+            </div>
+            <div class="nav-mobile">
+              <a href="#">Portals</a>
+              <ul>
+                <li>
+                  <a href="https://gymkhana.iitb.ac.in/~nss/greenopedia/home/"
+                    >Greenopedia</a
+                  >
+                </li>
+                <li>
+                  <a href="https://gymkhana.iitb.ac.in/~nss/olidwd/"
+                    >OLI Video Downloader</a
+                  >
+                </li>
+                <li>
+                  <a href="https://gymkhana.iitb.ac.in/~nss/nocs"
+                    >NOCS portal</a
+                  >
+                </li>
+                <li href="">
+                  <a>Make a Donation</a>
+                </li>
+                <li href="">
+                  <a>Nursery For All</a>
+                </li>
+                <li href="">
+                  <a>The Artistic Impact</a>
+                </li>
+              </ul>
+            </div>
+            <div class="nav-mobile">
+              <a href="#">Blogs</a>
+              <ul>
+                <li>
+                  <a
+                    href="https://nssiitbblog.wordpress.com/category/green-campus/prakriti/"
+                    >Prakriti</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="https://nssiitbblog.wordpress.com/category/parivartan/"
+                    >Parivartan</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="https://nssiitbblog.wordpress.com/category/jigyasa-a-quest-to-learn/"
+                    >Jigyasa</a
+                  >
+                </li>
+              </ul>
+            </div>
+            <div class="nav-mobile">
+              <a href="#">About Us</a>
+              <ul>
+                <li>
+                  <a href="#">Current Team</a>
+                </li>
+                <li>
+                  <a href="#">Media Coverage</a>
+                </li>
+                <li>
+                  <a href="https://gymkhana.iitb.ac.in/~nss/ContactUs.php">
+                    Contact Us
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="nav-mobile">
+              <a href="#">Impacting Lives</a>
+              <ul>
+                <li>
+                  <a href="#">Stories of change</a>
+                </li>
+                <li>
+                  <a href="#">My NSS experiences</a>
+                </li>
+                <li>
+                  <a href="https://gymkhana.iitb.ac.in/~nss/ContactUs.php">
+                    Invisible Humans of IIT Bombay
+                  </a>
+                </li>
+                <li><a>Memoirs</a></li>
+              </ul>
+            </div>
+            <div class="nav-mobile">
+              <a href="#">Resources</a>
+              <ul>
+                <li>
+                  <a href="#">Annual Reports</a>
+                </li>
+                <li>
+                  <a href="#">Words from OC</a>
+                </li>
+                <li>
+                  <a href="#">Gallery</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </nav>
+  <!-- <span>Not me but you</span> -->
+</section>
