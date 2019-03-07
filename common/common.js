07689b564dfab8c495299fd8620cb2474b5e3e9e
nautatva <nnautatva@gmail.com>
2019-03-08 00:15:10 +0530

    Navbar corrected for auto closing

--- common/common.js
+++ common/common.js
@@ -24,8 +24,41 @@ function includeHTML() {
           includeHTML();
         }
       };
+      xhttp.open('GET', file, true);
+      xhttp.send();
+
+      return;
+    }
+  }
+}
+
+function includeNav() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName('*');
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute('w3-include-nav');
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          // if (this.status == 404) {
+          //   elmnt.innerHTML = 'Page not found.';
+          // }
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute('w3-include-nav');
+        }
+      };
       xhttp.open('GET', file, false);
       xhttp.send();
+      
+      console.log($('.nav-mobile ul'));
 
       $(document).ready(function() {
         $('.nav-mobile ul').hide();
--- common/navbar/navbar.php
+++ common/navbar/navbar.php
@@ -1,5 +1,5 @@
 <script src="../common/jquery.min.js"></script>
 <script src="../common/common.js"></script>
 <!-- navbar -->
-<div w3-include-html="../common/navbar/navbar.html"></div>
+<div w3-include-nav="../common/navbar/navbar.html"></div>
 <!-- includeHTML() in last -->
