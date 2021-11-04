a9bf3ca22cb78778e2df1521bb6325703c32c329
Nautatva Navlakha <nnautatva@gmail.com>
2021-11-04 16:31:27 +0530

    mlc security compliance

--- olidwd/class.php
+++ olidwd/class.php
@@ -1,39 +1,55 @@
 <!DOCTYPE html>
+<?php 
+// Sanitize query parameters
+// mlc_security_compliance
+$_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
+?>
 <html lang="en-US">
    <head>
-    <meta charset="UTF-8"><link rel="shortcut icon" href="https://gymkhana.iitb.ac.in/~nss/assets/images/147455559147662-1-128x128-55.png" type="image/x-icon">
+    <meta charset="UTF-8"><link rel="shortcut icon" href="https://nss.iitb.ac.in/assets/images/147455559147662-1-128x128-55.png" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>All Class <?php echo $_GET["c"];?> Videos</title>
     <link rel="stylesheet" href="css/components.css">
     <link rel="stylesheet" href="css/icons.css">
     <link rel="stylesheet" href="css/responsee.css">
-    <link rel="stylesheet" href="https://gymkhana.iitb.ac.in/~nss/olidwd/owl-carousel/owl.carousel.css">
-    <link rel="stylesheet" href="https://gymkhana.iitb.ac.in/~nss/olidwd/owl-carousel/owl.theme.css">
+    <link rel="stylesheet" href="css/classPHP.css">
+    <link rel="stylesheet" href="https://nss.iitb.ac.in/olidwd/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" href="https://nss.iitb.ac.in/olidwd/owl-carousel/owl.theme.css">
+
+  <!-- navbar -->
+  <link rel="stylesheet" href="../olidwd/common/navbar/navbar.css">  
+  <script src="http://code.jquery.com/jquery-2.1.1.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="../olidwd/common/navbar/script.js"></script>
+<!-- end -->
+    
+<!-- Meta data -->
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="Open Learning Initiative" />
+<meta property="og:title" content="OLI | EO | NSS IIT-Bombay " />
+<meta name="og:description" content="Record of videos in regional languages, explaining the concepts that school students would be taught by a teacher."/>
+<meta property="og:image" content="https://nss.iitb.ac.in/olidwd/img/banner.jpg" />
+<meta property="og:image:width" content="800" />
+<meta property="og:image:height" content="450" />
+<meta property="og:url" content="https://nss.iitb.ac.in/olidwd/">
 
     <!-- CUSTOM STYLE -->
-    
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-    <script type="text/javascript" src="https://gymkhana.iitb.ac.in/~nss/olidwd/js/jquery-1.8.3.min.js"></script>
-    <script type="text/javascript" src="https://gymkhana.iitb.ac.in/~nss/olidwd/js/jquery-ui.min.js"></script>    
-    <script type="text/javascript" src="https://gymkhana.iitb.ac.in/~nss/olidwd/js/validation.js"></script> 
+    <script type="text/javascript" src="https://nss.iitb.ac.in/olidwd/js/jquery-1.8.3.min.js"></script>
+    <script type="text/javascript" src="https://nss.iitb.ac.in/olidwd/js/jquery-ui.min.js"></script>    
+    <script type="text/javascript" src="https://nss.iitb.ac.in/olidwd/js/validation.js"></script> 
     <style type="text/css">
       #tags ul li {
         display: inline;
       }
     </style>
-    
-<script> 
-$(function(){
-  $("#header").load("header.html"); 
-  
-});
-</script> 
  </head> 
   <body>
-  
-   <!-- HEADER -->
-  <div id="header"></div>
-    
+      <!-- navbar added -->
+  <?php include('./common/navbar/navbar.html'); ?>
+  <!-- end -->
+
     <!-- MAIN -->
     <main role="main">
       <!-- Main Carousel -->
@@ -41,17 +57,19 @@ $(function(){
         <div>
          
           <div class="item">
-            <div class="margin"> 
-              <div class="text-center text-size-30"> All Class <?php echo $_GET["c"];?> Videos </div>
-              <div class ="text-center  text-size-24">
+            <div class="homeHeadMain"> 
+              <div class="text-center text-size-30 homeHeadTitle"> All Class <?php echo $_GET["c"];?> Videos </div>
+              <div class ="text-center  text-size-24 homeHead">
              <?php   
               include('../../dbconfig.php');
             $conn= mysqli_connect($host, $username, $password, $db);
-            $class = $_GET['c'];
-            if($class == 9 || $class == 10)
-            $sql = "SELECT * FROM Folderlink WHERE (CLASS=".$class." OR CLASS='9&10')";
+            $conn->set_charset('utf8');
+            // mlc_security_compliance
+            $class = mysqli_real_escape_string($conn, $_GET['c']);
+            if($class === '9' || $class === '10')
+            $sql = "SELECT * FROM Folderlink WHERE (CLASS='".$class."' OR CLASS='9&10')";
             else 
-            $sql = "SELECT * FROM Folderlink WHERE CLASS=".$class;
+            $sql = "SELECT * FROM Folderlink WHERE CLASS='".$class."'";
             //echo $sql;
             $result = mysqli_query($conn, $sql);
             $i=0;
@@ -61,7 +79,7 @@ $(function(){
               Download Class '.$class.' '.$row["SUB"].' videos (in '.$row['LANG'].'). </a>
             ';
             if($i == 1)
-            echo '<br><br>';
+            echo ' ';
             $i = 1 - $i;
             }
 
@@ -72,19 +90,21 @@ $(function(){
                
               </div>
 </div>
-<br><br>
           
         <?php
 
             include('../../dbconfig.php');
             // if(is_set["page"])
             $conn= mysqli_connect($host, $username, $password, $db);
+            $conn->set_charset('utf8');
             if(!isset($_GET["page"])){
-              $class = $_GET['c'];
-              if($class == 9 || $class == 10)
-              $sql = "SELECT * FROM OLI WHERE (CLASS=".$class." OR CLASS='9&10')";
+              // mlc_security_compliance
+              $class = mysqli_real_escape_string($conn, $_GET['c']);
+
+              if($class === '9' || $class === '10')
+              $sql = "SELECT * FROM OLI WHERE (CLASS='".$class."' OR CLASS='9&10')";
               else 
-              $sql = "SELECT * FROM OLI WHERE CLASS=".$class;
+              $sql = "SELECT * FROM OLI WHERE CLASS='".$class."'";
               //echo $sql;
               $result = mysqli_query($conn, $sql);
 
@@ -93,15 +113,15 @@ $(function(){
             $page = 0;
             }
             else{
-            $page = $_GET["page"];
-            $rowcount = $_GET["t"];
+              $page = intval($_GET['page']);
+              $rowcount = mysqli_real_escape_string($conn, $_GET['t']);
             }
             $offset = $page * 6;
-            $class = $_GET['c'];
-            if($class == 9 || $class == 10)
-            $sql = "SELECT * FROM OLI WHERE (CLASS=".$class." OR CLASS='9&10') LIMIT $offset, 6";
+            $class = mysqli_real_escape_string($conn, $_GET['c']);
+            if($class === '9' || $class === '10')
+            $sql = "SELECT * FROM OLI WHERE (CLASS='".$class."' OR CLASS='9&10') LIMIT $offset, 6";
             else 
-            $sql = "SELECT * FROM OLI WHERE CLASS=".$class." LIMIT $offset, 6";
+            $sql = "SELECT * FROM OLI WHERE CLASS='".$class."' LIMIT $offset, 6";
             //echo $sql;
             $result = mysqli_query($conn, $sql);
            
@@ -110,21 +130,20 @@ $(function(){
                 $queryString = parse_url($row['YTUBE'], PHP_URL_QUERY);
                 parse_str($queryString, $params);
                 echo '
-                <div class="item">
-                <div class="margin"> 
-                <div class="s-12 m-12 l-12">
+                <div class="item homeHeadContent">
+                <div> 
+                <div class="s-12 m-12 l-12 headContentGradient">
                   <div class="image-border-radius margin-m-bottom">
-                    <div class="margin">
+                    <div>
                     <div class="s-12 m-12 l-2 margin-m-bottom"><p>&nbsp</p></div>
-                      <div class="s-12 m-12 l-2 margin-m-bottom">
-                        <a class="image-hover-zoom" href=video.php?id='.$row["ID"].'><img src="https://img.youtube.com/vi/'.$params["v"].'/mqdefault.jpg" alt=""></a>
+                      <div class="s-12 m-12 l-2 margin-m-bottom homeContentImgDiv">
+                        <a class="image-hover-zoom homeContentImg" href=video.php?id='.$row["ID"].'><img src="https://img.youtube.com/vi/'.$params["v"].'/mqdefault.jpg" alt="" style="padding-left: 0px;"></a>
                       </div>
                       <div class="s-12 m-12 l-6 margin-m-bottom">
-                        <a class="text-dark text-primary-hover" href=video.php?id='.$row["ID"].'><h3>'.$row['TITLE'].'</h3>
-                        <p>'.substr($row['DES'],0, 190).'...'.'</p>
-                        <p> Class '.$row['CLASS'].', '.$row['SUB'].', '.$row['LANG'].'</p></a>
+                        <a class="text-dark text-primary-hover headContentDetails" href=video.php?id='.$row["ID"].'><h3 class="homeContentTitle">'.$row['TITLE'].'</h3>
+                        <p>'.substr($row['DES'],0, 190).'...'.' <br>Class '.$row['CLASS'].', '.$row['SUB'].', '.$row['LANG'].'</p></a>
                     
-                       <a class =" text-primary-hover" target="_blank" href='.$row['DLINK'].'><i class="icon-download background-white iconx"></i>
+                       <a class ="headDownload text-primary-hover" target="_blank" href='.$row['DLINK'].'><i class="icon-download background-white iconx"></i>
                  &nbsp &nbsp Download The Video. </a>
                       </div>
                       <div class="s-12 m-12 l-2 margin-m-bottom"><p>&nbsp</p></div>
@@ -132,7 +151,7 @@ $(function(){
                   </div>
                 </div>
               
-            <p class="text-size-12">&nbsp<br><br></p></div>
+            <p class="text-size-12">&nbsp</p></div>
             </div>
               ';
             }
@@ -144,7 +163,7 @@ $(function(){
                          <div class=" margin-top-30 margin-bottom-0  text-center">
                          ';
                          if($page!=0)
-                         echo '<a href="class.php?c='.$_GET["c"].'&page='.($page - 1).'&t='.$rowcount.'"><button class="button background-primary border-radius text-white text-strong">< Previous Page </button></a>';
+                         echo '<a href="class.php?c='.urlencode($_GET["c"]).'&page='.($page - 1).'&t='.urlencode($rowcount) . '"><button class="button background-primary border-radius text-white text-strong">< Previous Page</button></a>';
                        echo'
                      </div>
                      </div>
@@ -152,7 +171,7 @@ $(function(){
                          <div class=" margin-top-30 margin-bottom-0  text-center">
                          ';
                          if($page!=floor($rowcount/6 ))
-                         echo '<a href="class.php?c='.$_GET["c"].'&page='.($page + 1).'&t='.$rowcount.'"><button class="button background-primary border-radius text-white text-strong"> Next Page > </button></a>';
+                         echo '<a href="class.php?c='.urlencode($_GET["c"]).'&page='.($page + 1).'&t='.urlencode($rowcount) . '"><button class="button background-primary border-radius text-white text-strong">Next Page ></button></a>';
                        echo'
                      </div>
                      </div>
@@ -164,28 +183,15 @@ $(function(){
              
                 
       </section>
-     
-    
-      
-     <section class="section background-white"><div class="s-12 m-12 l-12"></br><center><h3>To learn more about what <b>NSS, IIT Bombay</b> does, click <a href="https://gymkhana.iitb.ac.in/~nss"><b>here</b></a></h3></center></div> <!-- Section 1 --></section>
-    <section>
-    	
 
-    </section>
-    <!-- FOOTER -->
-    <footer> </center><section class="padding background-dark">
-        <div class="line">
-          <div class="s-12 l-6">
-            <p class="text-size-12">Copyright 2018, NSS IIT Bombay</p>
-            
-          </div>
-          
-        </div>
-      </section>
-    </footer>
-    <script type="text/javascript" src="https://gymkhana.iitb.ac.in/~nss/olidwd/js/responsee.js"></script>
-    <script type="text/javascript" src="https://gymkhana.iitb.ac.in/~nss/olidwd/owl-carousel/owl.carousel.js"></script>
-    <script type="text/javascript" src="https://gymkhana.iitb.ac.in/~nss/olidwd/js/template-scripts.js"></script>   
+      
+  <!-- footer -->
+  <script src="./common/common.js"></script>
+  <div w3-include-html="./common/footer/footer.html"></div>
+  <script>includeHTML();</script>
+    <script type="text/javascript" src="https://nss.iitb.ac.in/olidwd/js/responsee.js"></script>
+    <script type="text/javascript" src="https://nss.iitb.ac.in/olidwd/owl-carousel/owl.carousel.js"></script>
+    <script type="text/javascript" src="https://nss.iitb.ac.in/olidwd/js/template-scripts.js"></script>   
     
    </body>
    <link
--- olidwd/class_sub_lang.php
+++ olidwd/class_sub_lang.php
@@ -1,11 +1,17 @@
 <?php
-            $sub = $_GET['sub'];
-            $lang = $_GET['lang'];
-            $class = $_GET['class'];
+            // Sanitize query parameters
+            // mlc_security_compliance
+            $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRING);
+
             include('../../dbconfig.php');
-            
-             $conn= mysqli_connect($host, $username, $password, $db);
-            $sql = "SELECT * FROM Folderlink WHERE CLASS=".$class." AND SUB = '".$sub."' AND LANG = '".$lang."'";
+            $conn= mysqli_connect($host, $username, $password, $db);
+	    $conn->set_charset('utf8');
+
+            $sub = mysqli_real_escape_string($conn, $_GET['sub']);
+            $lang = mysqli_real_escape_string($conn, $_GET['lang']);
+	    $class = mysqli_real_escape_string($conn, $_GET['class']);
+
+            $sql = "SELECT * FROM Folderlink WHERE CLASS='".$class."' AND SUB='".$sub."' AND LANG='".$lang."'";
             $result = mysqli_query($conn, $sql);
             
             
@@ -23,7 +29,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>  <?php
-              if(($class == 9 || $class == 10) && $sub == "Maths"){
+              if(($class === '9' || $class === '10') && $sub == "Maths"){
                   echo "Class 9 and 10 Maths - " . $lang ." ";
               }
               else
@@ -34,44 +40,44 @@
     <link rel="stylesheet" href="css/components.css">
     <link rel="stylesheet" href="css/icons.css">
     <link rel="stylesheet" href="css/responsee.css">
-    <link rel="stylesheet" href="https://gymkhana.iitb.ac.in/~nss/olidwd/owl-carousel/owl.carousel.css">
-    <link rel="stylesheet" href="https://gymkhana.iitb.ac.in/~nss/olidwd/owl-carousel/owl.theme.css">
+    <link rel="stylesheet" href="css/classPHP.css">
+    <link rel="stylesheet" href="css/class_sub_langPHP.css">
+    <link rel="stylesheet" href="https://nss.iitb.ac.in/olidwd/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" href="https://nss.iitb.ac.in/olidwd/owl-carousel/owl.theme.css">
+
+<!-- Meta data -->
+<meta property="og:locale" content="en_US" />
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="Open Learning Initiative" />
+<meta property="og:title" content="OLI | EO | NSS IIT-Bombay " />
+<meta name="og:description" content="Record of videos in regional languages, explaining the concepts that school students would be taught by a teacher."/>
+<meta property="og:image" content="https://nss.iitb.ac.in/olidwd/img/banner.jpg" />
+<meta property="og:image:width" content="800" />
+<meta property="og:image:height" content="450" />
+<meta property="og:url" content="https://nss.iitb.ac.in/olidwd/">
 
     <!-- CUSTOM STYLE -->
-    
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-    <script type="text/javascript" src="https://gymkhana.iitb.ac.in/~nss/olidwd/js/jquery-1.8.3.min.js"></script>
-    <script type="text/javascript" src="https://gymkhana.iitb.ac.in/~nss/olidwd/js/jquery-ui.min.js"></script>    
-    <script type="text/javascript" src="https://gymkhana.iitb.ac.in/~nss/olidwd/js/validation.js"></script> 
+    <script type="text/javascript" src="https://nss.iitb.ac.in/olidwd/js/jquery-1.8.3.min.js"></script>
+    <script type="text/javascript" src="https://nss.iitb.ac.in/olidwd/js/jquery-ui.min.js"></script>    
+    <script type="text/javascript" src="https://nss.iitb.ac.in/olidwd/js/validation.js"></script> 
     <style type="text/css">
       #tags ul li {
         display: inline;
       }
     </style>
-    
-<script> 
-$(function(){
-  $("#header").load("header.html"); 
-  
-});
-</script> 
   </head> 
   <body>
-  
-   <!-- HEADER -->
-    <div id="header"></div>
-    
-    <!-- MAIN -->
-      <main role="main">
-      <!-- Main Carousel -->
-        
-      <section class="background-white">
+  <!-- navbar added -->
+  <?php include('./common/navbar/navbar.html'); ?>
+  <!-- end -->
+      <section class="background-white">  
         <div>
           <div class="item">
-            <div class="margin"> 
-              <div class="text-center text-size-30"> 
+            <div class="homeHeadMain"> 
+              <div class="text-center text-size-30 homeHeadTitle"> 
                 <?php
-                if(($class == 9 || $class == 10) && $sub == "Maths"){
+                if(($class === '9' || $class === '10') && $sub == "Maths"){
                     echo "Class 9 and 10 Maths - " . $lang;
                 }
                 else
@@ -79,7 +85,7 @@ $(function(){
                 
                 ?>
               </div>
-              <div class ="text-center text-primary-hover text-size-24"><a  target="_blank" href=<?php echo $playurl;?>><i class="icon-download  iconx"></i>
+              <div class ="text-center text-size-24  homeHead"><a  target="_blank" href=<?php echo $playurl;?>><i class="icon-download  iconx"></i>
                   Download The Entire Playlist. </a><br><br></div>
               
                 
@@ -91,12 +97,15 @@ $(function(){
             include('../../dbconfig.php');
            
             $conn= mysqli_connect($host, $username, $password, $db);
+            $conn->set_charset('utf8');
             if(!isset($_GET["page"])){
               
-              if($class == 9 || $class == 10)
-              $sql = "SELECT * FROM OLI WHERE (CLASS=".$class." OR CLASS='9&10') AND SUB = '".$sub."' AND LANG = '".$lang."'";
-              else 
-              $sql = "SELECT * FROM OLI WHERE CLASS=".$class." AND SUB='".$sub."' AND LANG='".$lang."'";
+              if($class === '9' || $class === '10') {
+		      $sql = "SELECT * FROM OLI WHERE (CLASS='".$class."' OR CLASS='9&10') AND SUB = '".$sub."' AND LANG = '".$lang."'";
+              }
+              else {
+		      $sql = "SELECT * FROM OLI WHERE CLASS='".$class."' AND SUB='".$sub."' AND LANG='".$lang."'";
+	      }
                 // echo $sql;
               $result = mysqli_query($conn, $sql);
 
@@ -105,17 +114,17 @@ $(function(){
             $page = 0;
             }
             else{
-            $page = $_GET["page"];
-            $rowcount = $_GET["t"];
+            $page = intval($_GET["page"]);
+            $rowcount = mysqli_real_escape_string($conn, $_GET["t"]);
             }
-            $offset = $page * 6;
-            
-            if($class == 9 || $class == 10)
-            $sql = "SELECT * FROM OLI WHERE (CLASS=".$class." OR CLASS='9&10') AND SUB = '".$sub."' AND LANG = '".$lang."' LIMIT $offset, 6";
-            else 
-            $sql = "SELECT * FROM OLI WHERE CLASS=".$class." AND SUB='".$sub."' AND LANG='".$lang."' LIMIT $offset, 6";
-            $result = mysqli_query($conn, $sql);
+	    $offset = $page * 6;
 
+            if($class === '9' || $class === '10') {
+		    $sql = "SELECT * FROM OLI WHERE (CLASS='".$class."' OR CLASS='9&10') AND SUB = '".$sub."' AND LANG = '".$lang."' LIMIT $offset, 6";
+	    } else { 
+		    $sql = "SELECT * FROM OLI WHERE CLASS='".$class."' AND SUB='".$sub."' AND LANG='".$lang."' LIMIT $offset, 6";
+	    }
+            $result = mysqli_query($conn, $sql);
 
             while ($row = mysqli_fetch_assoc($result)) {
             //echo $row;   
@@ -127,20 +136,20 @@ $(function(){
                 //var_dump($params);
                 
               //echo 'https://img.youtube.com/vi/'.$params["v"].'/mqdefault.jpg';
-            echo '
-                <div class="s-12 m-12 l-12">
+            echo '<div class="item homeHeadContent">
+                <div class="s-12 m-12 l-12 headContentGradient">
                   <div class="image-border-radius margin-m-bottom">
-                    <div class="margin">
+                    <div>
                     <div class="s-12 m-12 l-2 margin-m-bottom"><p>&nbsp</p></div>
-                      <div class="s-12 m-12 l-2 margin-m-bottom">
-                        <a class="image-hover-zoom" href=video.php?id='.$row["ID"].'><img src="https://img.youtube.com/vi/'.$params["v"].'/mqdefault.jpg" alt=""></a>
+                      <div class="s-12 m-12 l-2 margin-m-bottom homeContentImgDiv">
+                        <a class="image-hover-zoom homeContentImg" href=video.php?id='.$row["ID"].'><img src="https://img.youtube.com/vi/'.$params["v"].'/mqdefault.jpg" alt="" style="padding-left: 0px;"></a>
                       </div>
                       <div class="s-12 m-12 l-6 margin-m-bottom">
-                        <a class="text-dark text-primary-hover" href=video.php?id='.$row["ID"].'><h3>'.$row['TITLE'].'</h3>
+                        <a class="text-dark text-primary-hover headContentDetails" href=video.php?id='.$row["ID"].'><h3 class="homeContentTitle">'.$row['TITLE'].'</h3>
                         <p>'.substr($row['DES'],0,190).'...'.'</p>
                         <p> Class '.$row['CLASS'].', '.$sub.', '.$lang.'</p></a>
                     
-                       <a class =" text-primary-hover" target="_blank" href='.$row['DLINK'].'><i class="icon-download background-white iconx"></i>
+                       <a class ="headDownload text-primary-hover" target="_blank" href='.$row['DLINK'].'><i class="icon-download background-white iconx"></i>
                  &nbsp &nbsp Download The Video. </a>
                       </div>
                       <div class="s-12 m-12 l-2 margin-m-bottom"><p>&nbsp</p></div>
@@ -162,7 +171,7 @@ $(function(){
                         <div class=" margin-top-30 margin-bottom-0  text-center">
                         ';
                         if($page!=0)
-                        echo '<a href="class_sub_lang.php?class='.$_GET["class"].'&sub='.$_GET["sub"].'&lang='.$_GET["lang"].'&page='.($page - 1).'&t='.$rowcount.'"><button class="button background-primary border-radius text-white text-strong">< Previous Page </button></a>';
+                        echo '<a href="class_sub_lang.php?class='.urlencode($_GET["class"]).'&sub='.urlencode($_GET["sub"]).'&lang='.urlencode($_GET["lang"]).'&page='.($page - 1).'&t='.urlencode($rowcount) . '"><button class="button background-primary border-radius text-white text-strong">< Previous Page </button></a>';
                       echo'
                     </div>
                     </div>
@@ -170,7 +179,7 @@ $(function(){
                         <div class=" margin-top-30 margin-bottom-0  text-center">
                         ';
                         if($page!=floor($rowcount/6 ))
-                        echo '<a href="class_sub_lang.php?class='.$_GET["class"].'&sub='.$_GET["sub"].'&lang='.$_GET["lang"].'&page='.($page + 1).'&t='.$rowcount.'"><button class="button background-primary border-radius text-white text-strong"> Next Page > </button></a>';
+                        echo '<a href="class_sub_lang.php?class='.urlencode($_GET["class"]).'&sub='.urlencode($_GET["sub"]).'&lang='.urlencode($_GET["lang"]).'&page='.($page + 1).'&t='.urlencode($rowcount) . '"><button class="button background-primary border-radius text-white text-strong"> Next Page > </button></a>';
                       echo'
                     </div>
                     </div>
@@ -195,26 +204,19 @@ $(function(){
         </div> -->
           
              </section>
-
-    
-     
-    
-      
-      <!-- Section 1 -->
-      <section class="section background-white"><div class="s-12 m-12 l-12"></br><center><h3>To learn more about what <b>NSS, IIT Bombay</b> does, click <a href="https://gymkhana.iitb.ac.in/~nss"><b>here</b></a></h3></center></div> <!-- Section 1 --></section>
-      <section>
-    <!-- FOOTER -->
-    <footer> <section class="padding background-dark">
-        <div class="line">
-          <div class="s-12 l-6">
-          <br>
-            <p class="text-size-12">Copyright 2018, NSS IIT Bombay</p>
-            
-          </div>
-          
-        </div>
-      </section>
-    </footer>
+  <!-- footer -->
+  <script src="./common/common.js"></script>
+  <div w3-include-html="./common/footer/footer.html"></div>
+  <script>includeHTML();</script>
    
    </body>
+   <link
+ href="https://fonts.googleapis.com/css?family=Montserrat"
+ rel="stylesheet"
+/>
+<style>
+ body{
+   font-family: 'Montserrat', sans-serif;
+ }
+</style>
 </html>
